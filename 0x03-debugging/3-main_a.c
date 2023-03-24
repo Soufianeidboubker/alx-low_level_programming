c600f4b4498ce1ebf59fe13620b4366b5d98d993
soufianeidboubker <soufianeidboubker@gmail.com>
2023-03-24 10:10:03 -0700

    task 3

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -2,10 +2,10 @@
 #include "main.h"
 
 /**
-* main - takes a date and prints how many days are left in the year, taking
-* leap years into account
-* Return: 0
-*/
+ * main - takes a date and prints how many days are left in the year, taking
+ * leap years into account
+ * Return: 0
+ */
 
 int main(void)
 {
@@ -13,15 +13,15 @@ int main(void)
 	int day;
 	int year;
 
-    month = 4;
-    day = 01;
-    year = 1997;
-    
-    printf("Date: %02d/%02d/%04d\n", month, day, year);
-    
-    day = convert_day(month, day);
-    
-    print_remaining_days(month, day, year);
-    
-    return (0);
+	month = 4;
+	day = 01;
+	year = 1997;
+
+	printf("Date: %02d/%02d/%04d\n", month, day, year);
+
+	day = convert_day(month, day);
+
+	print_remaining_days(month, day, year);
+
+	return (0);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,9 +5,9 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int a, int c);
+int largest_number(int a, int b, int c);
 int convert_day(int mounth, int day);
 void print_remaining_days(int mounth, int day, int year);
+void print_times_table(int n);
 
-
-#endif /* MAIN_H */
+#endif
