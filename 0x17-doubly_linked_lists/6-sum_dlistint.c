f750a42e1031cc7addfb5407781870f6e58cc497
soufianeidboubker <soufianeidboubker@gmail.com>
2023-08-17 08:19:00 -0700

    updates

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * s_dlistint - returns the s of all the data (n)
+ * of a doubly linked list
+ *
+ * @head: head of the list
+ * Return: s of the data
+ */
+int s_dlistint(dlistint_t *head)
+{
+	int s;
+
+	s = 0;
+
+	if (head != NULL)
+	{
+		while (head->prev != NULL)
+			head = head->prev;
+
+		while (head != NULL)
+		{
+			s += head->n;
+			head = head->next;
+		}
+	}
+
+	return (s);
+}
